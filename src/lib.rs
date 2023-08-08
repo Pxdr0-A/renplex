ce22d3830690a2b93a257877beda7f6558763754
Pedro Alves <pedromfa98@gmail.com>
2023-08-08 15:15:31 +0100

    testedactivation macro successfully

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,19 @@ pub mod prelude;
 
 #[cfg(test)]
 mod tests {
-    
+  use super::*;
+
+  #[test]
+  fn value_activation() {
+    // temporary test
+    // afterwards change vars to private
+    use prelude::neuron::activation::Activation;
+    use prelude::neuron::activation::ActivationFunction;
+
+    let a: f32 = 0.34;
+    let func = ActivationFunction::SIGMOID;
+
+    let result = a.activation(func);
+    println!("{}", result);
+  } 
 }
