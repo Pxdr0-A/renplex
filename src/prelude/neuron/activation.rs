ae9da59a704c314c4c37588de1b992839a115b8b
Pedro Alves <pedromfa98@gmail.com>
2023-08-08 15:14:56 +0100

    acitvation mod - macro for general type activation

---
+++ src/prelude/neuron/activation.rs
@@ -0,0 +1,37 @@
+pub enum ActivationFunction {
+  SIGMOID,
+  TANH,
+  RELU
+}
+
+pub trait Activation {
+  fn activation(&self, func: ActivationFunction) -> Self;
+}
+
+macro_rules! activate_me {
+  ($value:ident, $func:ident) => {
+    match $func {
+      ActivationFunction::SIGMOID => {
+        1.0 / (1.0 + (-$value).exp())
+      },
+      ActivationFunction::TANH => {
+        $value.tanh()
+      },
+      ActivationFunction::RELU => {
+        if $value.is_sign_positive() { *$value } else { 0.0 }
+      }
+    } 
+  };
+}
+
+impl Activation for f32 {
+  fn activation(&self, func: ActivationFunction) -> f32 {
+    activate_me!(self, func)
+  }
+}
+
+impl Activation for f64 {
+  fn activation(&self, func: ActivationFunction) -> f64 {
+    activate_me!(self, func)
+  }
+}
