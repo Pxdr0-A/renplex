d03e6f5c95fe6d8cdb034f25e9093c9d15d95766
Pedro Alves <pedromfa98@gmail.com>
2023-08-08 15:15:10 +0100

    restructure

--- src/prelude/neuron/mod.rs
+++ src/prelude/neuron/mod.rs
@@ -1,12 +1,10 @@
+pub mod activation;
+
+use activation::ActivationFunction;
+
 pub struct Neuron<W> {
   pub id: usize,
   pub weights: Vec<W>,
   pub bias: W,
   pub activation: ActivationFunction
-}
-
-pub enum ActivationFunction {
-  SIGMOID,
-  TANH,
-  RELU
 }
