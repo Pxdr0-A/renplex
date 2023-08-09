a410153a7cad011e0bfbc3e7e82fb7ef02abff5c
Pedro Alves <pedromfa98@gmail.com>
2023-08-10 00:26:10 +0100

    guiding comments

--- src/prelude/neuron/activation.rs
+++ src/prelude/neuron/activation.rs
@@ -24,6 +24,11 @@ macro_rules! activate_me {
   };
 }
 
+// To implement Activation to a custom type, those types need to have
+// minus trait (negative value)
+// exp(), tanh(), is_sign_positive() methods.
+
+// UPDATE THIS CODE TO USE A MACRO!
 impl Activation for f32 {
   fn activation(&self, func: ActivationFunction) -> f32 {
     activate_me!(self, func)
@@ -35,3 +40,5 @@ impl Activation for f64 {
     activate_me!(self, func)
   }
 }
+
+// Activation implementation for Cfloat (for f32 and f64)
