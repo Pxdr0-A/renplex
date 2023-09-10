cbd81702c0e7827fec869cea3bb85c1da2aeb468
Pedro Alves <pedromfa98@gmail.com>
2023-09-10 22:54:33 +0100

    done with generic signal function

--- src/prelude/neuron/mod.rs
+++ src/prelude/neuron/mod.rs
@@ -1,6 +1,11 @@
 pub mod activation;
 
-use activation::ActivationFunction;
+
+//std
+use std::ops::{AddAssign, Mul};
+
+// local
+use self::activation::{ActivationFunction, Activation};
 
 
 pub struct Neuron<W> {
@@ -24,5 +29,21 @@ impl<W> Neuron<W> {
             bias, 
             activation 
         }
+    }
+
+    pub fn signal(&self, input: &[W]) -> W 
+        where W: AddAssign + Mul<Output = W> + Activation, W: Copy {
+        
+        assert_eq!(self.weights.len(), input.len(),
+                   "Input length must match the number of neuron inputs."
+        );
+
+        // init cycle (bias needs to come with the proper sign)
+        let mut out = self.bias;
+        for i in 0..input.len() {
+            out += self.weights[i] * input[i];
+        }
+
+        out.activation(&self.activation)
     }
 }
