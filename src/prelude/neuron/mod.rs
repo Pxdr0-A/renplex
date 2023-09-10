4759a903365f6c23650cecdbd6af58288cf84ddd
Pedro Alves <pedromfa98@gmail.com>
2023-09-10 20:35:47 +0100

    begining to tackle neuron struct

--- src/prelude/neuron/mod.rs
+++ src/prelude/neuron/mod.rs
@@ -2,9 +2,27 @@ pub mod activation;
 
 use activation::ActivationFunction;
 
+
 pub struct Neuron<W> {
     pub id: usize,
     pub weights: Vec<W>,
     pub bias: W,
     pub activation: ActivationFunction,
+}
+
+impl<W> Neuron<W> {
+    pub fn new(
+        id: usize, 
+        weights: Vec<W>,
+        bias: W, 
+        activation: ActivationFunction
+    ) -> Neuron<W> {
+        
+        Neuron { 
+            id, 
+            weights, 
+            bias, 
+            activation 
+        }
+    }
 }
