a31cfa217b4e36e704a9993d7c657d7904c83761
Pedro Alves <pedromfa98@gmail.com>
2023-10-13 00:14:55 +0100

    cast floats to cfloats

---
+++ src/math/complex/casts.rs
@@ -0,0 +1,23 @@
+use super::Cfloat;
+
+pub trait ComplexCast<P> {
+    fn to_complex(self) -> Cfloat<P>;
+}
+
+impl ComplexCast<f32> for f32 {
+    fn to_complex(self) -> Cfloat<f32> {
+        Cfloat {
+            x: self, 
+            y: 0.0 
+        }
+    }
+}
+
+impl ComplexCast<f64> for f64 {
+    fn to_complex(self) -> Cfloat<f64> {
+        Cfloat {
+            x: self, 
+            y: 0.0 
+        }
+    }
+}
