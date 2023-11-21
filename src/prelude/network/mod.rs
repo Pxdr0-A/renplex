bde55d9529e02e063949f114e54aeffe6221f42e
Pedro Alves <pedromfa98@gmail.com>
2023-11-21 20:41:35 +0000

    macros for dense network initialization

--- src/prelude/network/mod.rs
+++ src/prelude/network/mod.rs
@@ -29,8 +29,10 @@ pub struct DenseNetwork<W> {
 
 impl<W> DenseNetwork<W> 
     where 
-        W: AddAssign + Mul<Output = W> + Activatable, 
-        W: Copy  {
+        W: AddAssign + Mul<Output = W> + Neg<Output = W> + PartialEq, 
+        W: Activatable + Number,
+        W: Copy,
+        W: Debug {
 
     /// Returns a `DenseNetwork<W>` with just input. Reallocation will happen everytime a layer is added.
     /// 
@@ -89,12 +91,86 @@ impl<W> DenseNetwork<W>
     }
 }
 
+impl<P> DenseNetwork<P> 
+    where 
+        P: Add<Output=P> + Sub<Output=P> + Neg<Output=P> + Mul<Output=P> + Div<Output=P>,
+        P: AddAssign,
+        P: Activatable + Trignometricable + Arcable + Powerable + SquareRootable,
+        P: Number + PartialEq,
+        P: Debug,
+        P: Copy + Debug  {
+
+    pub fn fit(
+        &self, 
+        data: Dataset<P, u8>
+    ) {
+
+        let dummy_array = data.target.clone();
+        let mut degree = dummy_array
+            .into_iter()
+            .max()
+            .unwrap();
+        
+        degree += 1;
+
+        let mut current_row;
+        let mut out;
+        
+        let mut processed_out: Matrix<P> = Matrix::new(
+            [data.body.shape[0], degree as usize]
+        );
+        
+        let mut expected_out: Matrix<P> = Matrix::new(
+            [data.body.shape[0], degree as usize]
+        );
+
+        let mut expectancy_model;
+
+        // going through all data to collect predictions
+        for row in 0..data.body.shape[0] {
+
+            current_row = data.body.row(&row);
+
+            out = self.forward(current_row);
+
+            // write a match for all the critiria
+            processed_out.add_row(
+                &mut out
+            );
+
+            // consider adding gaussian for building this vector
+            // or some other stuff
+            expectancy_model = vec![
+                P::null(); 
+                degree as usize
+            ];
+            // procedure only valid for classification
+            expectancy_model[data.target[row] as usize] += P::unit();
+            expected_out.add_row(&mut expectancy_model);
+        }
+
+        println!("Expected.");
+        println!("{:?}", expected_out);
+        println!("Obtained.");
+        println!("{:?}", processed_out);
+
+        // make a general cost function interface
+        let cost_function = (processed_out - expected_out).powi(2i32);
+
+        println!("Cost Function.");
+        println!("{:?}", cost_function);
+
+    }
+
+}
+
 impl<P> DenseNetwork<Cfloat<P>> 
     where 
         P: Add<Output=P> + Sub<Output=P> + Neg<Output=P> + Mul<Output=P> + Div<Output=P>,
         P: AddAssign,
         P: Activatable + Trignometricable + Arcable + Powerable + SquareRootable,
-        P: ComplexCast<P> + Number,
+        P: ComplexCast<P> + Number + PartialEq,
+        P: Debug,
         Cfloat<P>: Activatable,
         P: Copy + Debug  {
 
@@ -135,7 +211,7 @@ impl<P> DenseNetwork<Cfloat<P>>
             current_row = complex_data.body.row(&row);
 
             out = self.forward(current_row);
-            
+
             selection_out = match critiria {
                 
                 ComplexCritiria::REAL => { 
@@ -183,9 +259,9 @@ impl<P> DenseNetwork<Cfloat<P>>
             expected_out.add_row(&mut expectancy_model);
         }
 
-        println!("Expected.");
-        println!("{:?}", expected_out);
-        println!("Obtained.");
+        //println!("Expected.");
+        //println!("{:?}", expected_out);
+        //println!("Obtained.");
         println!("{:?}", processed_out);
 
         // make a general cost function interface
@@ -198,141 +274,277 @@ impl<P> DenseNetwork<Cfloat<P>>
 
 }
 
-impl DenseNetwork<Cfloat<f32>> {
-    pub fn init(
-        input_length: usize,
-        n_units: usize,
-        activation: ActivationFunction,
-        re_weight_scale: f32,
-        im_weight_scale: f32,
-        re_bias_scale: f32,
-        im_bias_scale: f32,
-        seed: &mut u128
-    ) -> DenseNetwork<Cfloat<f32>> {
-
-        let order: usize = 0;
-        let n_weights = match calc_n_weights(input_length, n_units) {
+macro_rules! init_real_dense_network {
+    ($($generator: ident, $float: ty), *) => {
+        $(
+            impl DenseNetwork<$float> {
+                pub fn init(
+                    input_length: usize,
+                    n_units: usize,
+                    activation: ActivationFunction,
+                    weight_scale: $float,
+                    bias_scale: $float,
+                    seed: &mut u128
+                ) -> DenseNetwork<$float> {
             
-            Ok(number) => number,
+                    let order: usize = 0;
+                    let n_weights = match calc_n_weights(input_length, n_units) {
+                        
+                        Ok(number) => number,
+                        
+                        Err((input, cells)) => {
+                            panic!(
+                                "Inconsistent relation between input and number of units. 
+                                Input length {input} is not divisable by {cells}."
+                            );
+                        }
+                    };
             
-            Err((input, cells)) => {
-                panic!(
-                    "Inconsistent relation between input and number of units. 
-                     Input length {input} is not divisable by {cells}."
-                );
-            }
-        };
-
-        let mut network = DenseNetwork::<Cfloat<f32>>::new(n_units);
-        
-        network.add_rnd_param_layer(
-            n_units, 
-            n_weights, 
-            activation, 
-            re_weight_scale, 
-            im_weight_scale, 
-            re_bias_scale, 
-            im_bias_scale,
-            order,
-            seed
-        );
-
-        network
-    }
-
-    pub fn add(
-        &mut self, 
-        n_units: usize,
-        activation: ActivationFunction,
-        re_weight_scale: f32,
-        im_weight_scale: f32,
-        re_bias_scale: f32,
-        im_bias_scale: f32,
-        seed: &mut u128
-    ) {
-    
-        // fully connected layer
-        let n_weights = {
-
-            if self.hidden_layers.len() == 0 {
-
-                self.input_layer
-                    .units
-                    .len()
-
-            } else {
-
-                self.hidden_layers
-                    .last()
-                    .unwrap()
-                    .units
-                    .len()
-
-            }
-
-        };
-
-        // allocation of memory related to the hidden layer
-        self.add_layer(HiddenLayer::new(n_units));
-        let order = self.hidden_layers.len();
-
-        self.add_rnd_param_layer(
-            n_units, 
-            n_weights, 
-            activation, 
-            re_weight_scale, 
-            im_weight_scale, 
-            re_bias_scale, 
-            im_bias_scale,
-            order,
-            seed
-        )
-
-    }
-
-    fn add_rnd_param_layer(
-        &mut self,
-        n_units: usize,
-        n_weights: usize,
-        activation: ActivationFunction,
-        re_weight_scale: f32,
-        im_weight_scale: f32,
-        re_bias_scale: f32,
-        im_bias_scale: f32,
-        order: usize,
-        seed: &mut u128
-    ) {
-
-        let mut current_weights: Vec<Cfloat<f32>> = Vec::with_capacity(n_weights);
-
-        for _ in 0..n_units {
-            for _ in 0..n_weights {
-                current_weights.push(
-                    Cfloat::<f32>::new(
-                        lcgf32(seed) * re_weight_scale, 
-                        lcgf32(seed) * im_weight_scale
+                    let mut network = DenseNetwork::<$float>::new(n_units);
+                    
+                    network.add_rnd_param_layer(
+                        n_units, 
+                        n_weights, 
+                        activation, 
+                        weight_scale, 
+                        bias_scale,
+                        order,
+                        seed
+                    );
+            
+                    network
+                }
+            
+                pub fn add(
+                    &mut self, 
+                    n_units: usize,
+                    activation: ActivationFunction,
+                    weight_scale: $float,
+                    bias_scale: $float,
+                    seed: &mut u128
+                ) {
+                
+                    // fully connected layer
+                    let n_weights = {
+            
+                        if self.hidden_layers.len() == 0 {
+            
+                            self.input_layer
+                                .units
+                                .len()
+            
+                        } else {
+            
+                            self.hidden_layers
+                                .last()
+                                .unwrap()
+                                .units
+                                .len()
+            
+                        }
+            
+                    };
+            
+                    // allocation of memory related to the hidden layer
+                    self.add_layer(HiddenLayer::new(n_units));
+                    let order = self.hidden_layers.len();
+            
+                    self.add_rnd_param_layer(
+                        n_units, 
+                        n_weights, 
+                        activation, 
+                        weight_scale,
+                        bias_scale,
+                        order,
+                        seed
                     )
-                );
+            
+                }
+            
+                fn add_rnd_param_layer(
+                    &mut self,
+                    n_units: usize,
+                    n_weights: usize,
+                    activation: ActivationFunction,
+                    weight_scale: $float,
+                    bias_scale: $float,
+                    order: usize,
+                    seed: &mut u128
+                ) {
+            
+                    let mut current_weights: Vec<$float> = Vec::with_capacity(n_weights);
+            
+                    for _ in 0..n_units {
+                        for _ in 0..n_weights {
+                            current_weights.push(
+                                $generator(seed) * weight_scale
+                            );
+                        }
+            
+                        self.add_unit(
+                            order,
+                            Neuron::new(
+                                current_weights.clone(), 
+                                $generator(seed) * bias_scale,
+                                activation.clone()
+                            )
+                        );
+            
+                        current_weights.clear();
+                    }
+            
+                }
+            
             }
-
-            self.add_unit(
-                order,
-                Neuron::new(
-                    current_weights.clone(), 
-                    Cfloat::<f32>::new(
-                        lcgf32(seed) * re_bias_scale, 
-                        lcgf32(seed) * im_bias_scale
-                    ), 
-                    activation.clone()
-                )
-            );
-
-            current_weights.clear();
-        }
-
-    }
-
+        )*
+    };
 }
+
+init_real_dense_network!(lcgf32, f32, lcgf64, f64);
+
+macro_rules! init_complex_dense_network {
+    ($($generator: ident, $float: ty), *) => {
+        $(
+            impl DenseNetwork<Cfloat<$float>> {
+                pub fn init(
+                    input_length: usize,
+                    n_units: usize,
+                    activation: ActivationFunction,
+                    re_weight_scale: $float,
+                    im_weight_scale: $float,
+                    re_bias_scale: $float,
+                    im_bias_scale: $float,
+                    seed: &mut u128
+                ) -> DenseNetwork<Cfloat<$float>> {
+            
+                    let order: usize = 0;
+                    let n_weights = match calc_n_weights(input_length, n_units) {
+                        
+                        Ok(number) => number,
+                        
+                        Err((input, cells)) => {
+                            panic!(
+                                "Inconsistent relation between input and number of units. 
+                                Input length {input} is not divisable by {cells}."
+                            );
+                        }
+                    };
+            
+                    let mut network = DenseNetwork::<Cfloat<$float>>::new(n_units);
+                    
+                    network.add_rnd_param_layer(
+                        n_units, 
+                        n_weights, 
+                        activation, 
+                        re_weight_scale, 
+                        im_weight_scale, 
+                        re_bias_scale, 
+                        im_bias_scale,
+                        order,
+                        seed
+                    );
+            
+                    network
+                }
+            
+                pub fn add(
+                    &mut self, 
+                    n_units: usize,
+                    activation: ActivationFunction,
+                    re_weight_scale: $float,
+                    im_weight_scale: $float,
+                    re_bias_scale: $float,
+                    im_bias_scale: $float,
+                    seed: &mut u128
+                ) {
+                
+                    // fully connected layer
+                    let n_weights = {
+            
+                        if self.hidden_layers.len() == 0 {
+            
+                            self.input_layer
+                                .units
+                                .len()
+            
+                        } else {
+            
+                            self.hidden_layers
+                                .last()
+                                .unwrap()
+                                .units
+                                .len()
+            
+                        }
+            
+                    };
+            
+                    // allocation of memory related to the hidden layer
+                    self.add_layer(HiddenLayer::new(n_units));
+                    let order = self.hidden_layers.len();
+            
+                    self.add_rnd_param_layer(
+                        n_units, 
+                        n_weights, 
+                        activation, 
+                        re_weight_scale, 
+                        im_weight_scale, 
+                        re_bias_scale, 
+                        im_bias_scale,
+                        order,
+                        seed
+                    )
+            
+                }
+            
+                fn add_rnd_param_layer(
+                    &mut self,
+                    n_units: usize,
+                    n_weights: usize,
+                    activation: ActivationFunction,
+                    re_weight_scale: $float,
+                    im_weight_scale: $float,
+                    re_bias_scale: $float,
+                    im_bias_scale: $float,
+                    order: usize,
+                    seed: &mut u128
+                ) {
+            
+                    let mut current_weights: Vec<Cfloat<$float>> = Vec::with_capacity(n_weights);
+            
+                    for _ in 0..n_units {
+                        for _ in 0..n_weights {
+                            current_weights.push(
+                                Cfloat::<$float>::new(
+                                    $generator(seed) * re_weight_scale, 
+                                    $generator(seed) * im_weight_scale
+                                )
+                            );
+                        }
+            
+                        self.add_unit(
+                            order,
+                            Neuron::new(
+                                current_weights.clone(), 
+                                Cfloat::<$float>::new(
+                                    $generator(seed) * re_bias_scale, 
+                                    $generator(seed) * im_bias_scale
+                                ), 
+                                activation.clone()
+                            )
+                        );
+            
+                        current_weights.clear();
+                    }
+            
+                }
+            
+            }
+        )*
+    };
+}
+
+init_complex_dense_network!(lcgf32, f32, lcgf64, f64);
 
 /// Small utility that calculates the number of weights needed for
 /// a dense networks's input.
