8ce9b205aea74f69ec2549620c7697d92f137dad
Pedro Alves <pedromfa98@gmail.com>
2024-03-01 00:53:13 +0000

    worked on layer initialization

--- src/rvnn/layer/conv.rs
+++ src/rvnn/layer/conv.rs
@@ -1,7 +1,7 @@
 use crate::math::matrix::Matrix;
 use crate::input::{InputType, OutputType};
 
-use super::{LayerError, LayerLike};
+use super::{InitError, LayerError, LayerLike};
 
 pub struct ConvLayer<T> {
   _weights: Matrix<T>,
@@ -9,8 +9,15 @@ pub struct ConvLayer<T> {
 }
 
 impl<T> LayerLike<T> for ConvLayer<T> {
+  fn init(inputs: usize, units: usize, func: crate::act::ActFunc, method: super::InitMethod, seed: &mut u128) -> Result<Self, InitError> {
+    unimplemented!()
+  }
 
   fn forward(&self, _input_type: InputType<T>) -> Result<OutputType<T>, LayerError> {
+    unimplemented!()
+  }
+
+  fn wrap(self) -> super::Layer<T> {
     unimplemented!()
   }
 }
--- src/rvnn/layer/dense.rs
+++ src/rvnn/layer/dense.rs
@@ -3,7 +3,8 @@ use crate::math::matrix::{Matrix, SliceOps};
 use crate::math::{BasicOperations, Real};
 use crate::input::{InputType, OutputType};
 
-use super::{LayerError, LayerLike};
+use super::{Layer, LayerError, LayerLike, InitMethod, InitError};
+
 
 pub struct DenseLayer<T> {
   weights: Matrix<T>,
@@ -14,6 +15,30 @@ pub struct DenseLayer<T> {
 /* the activation function is what makes this layer real or complex */
 /* the implementations are almost the same tho */
 impl<T: Real + BasicOperations<T>> LayerLike<T> for DenseLayer<T> {
+  fn init(inputs: usize, units: usize, func: ActFunc, method: InitMethod, seed: &mut u128) -> Result<Self, InitError> {
+    let mut body = Vec::with_capacity(units * inputs);
+    let mut biases = Vec::with_capacity(units);
+
+    match method {
+      InitMethod::Random => {
+        for _ in 0..units {
+          for _ in 0..inputs {
+            body.push(T::gen(seed));
+          }
+          biases.push(T::gen(seed));
+        }
+      },
+      _ => { return Err(InitError::InvalidMethod) }
+    }
+
+    Ok(
+      DenseLayer {
+        weights: Matrix::from_body(body, [units, inputs]),
+        biases,
+        func
+      }
+    )
+  }
 
   fn forward(&self, input_type: InputType<T>) -> Result<OutputType<T>, LayerError> {
     match input_type {
@@ -39,5 +64,9 @@ impl<T: Real + BasicOperations<T>> LayerLike<T> for DenseLayer<T> {
       },
       _ => { Err(LayerError::InvalidInput) }
     }
+  }
+
+  fn wrap(self) -> Layer<T> {
+    Layer::Dense(self)
   }
 }
