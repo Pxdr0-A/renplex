a100051f61e2fa42419513d7e12be6105e00a19f
Pedro Alves <pedromfa98@gmail.com>
2024-03-02 00:26:58 +0000

    added more trait methods for LayerLike and more Layer interface ops

--- src/rvnn/layer/dense.rs
+++ src/rvnn/layer/dense.rs
@@ -1,9 +1,9 @@
 use crate::act::ActFunc;
 use crate::math::matrix::{Matrix, SliceOps};
 use crate::math::{BasicOperations, Real};
-use crate::input::{InputType, OutputType};
+use crate::input::{InputShape, InputType, OutputShape, OutputType};
 
-use super::{Layer, LayerError, LayerLike, InitMethod, InitError};
+use super::{Layer, LayerForwardError, LayerLike, InitMethod, LayerInitError};
 
 
 pub struct DenseLayer<T> {
@@ -15,32 +15,106 @@ pub struct DenseLayer<T> {
 /* the activation function is what makes this layer real or complex */
 /* the implementations are almost the same tho */
 impl<T: Real + BasicOperations<T>> LayerLike<T> for DenseLayer<T> {
-  fn init(inputs: usize, units: usize, func: ActFunc, method: InitMethod, seed: &mut u128) -> Result<Self, InitError> {
-    let mut body = Vec::with_capacity(units * inputs);
-    let mut biases = Vec::with_capacity(units);
-
-    match method {
-      InitMethod::Random => {
-        for _ in 0..units {
-          for _ in 0..inputs {
-            body.push(T::gen(seed));
-          }
-          biases.push(T::gen(seed));
-        }
-      },
-      _ => { return Err(InitError::InvalidMethod) }
+  fn is_empty(&self) -> bool {
+    if (self.weights.get_shape() == [0_usize, 0]) && (self.biases.len() == 0) {
+      true
+    } else {
+      false
     }
-
-    Ok(
-      DenseLayer {
-        weights: Matrix::from_body(body, [units, inputs]),
-        biases,
-        func
-      }
-    )
   }
 
-  fn forward(&self, input_type: InputType<T>) -> Result<OutputType<T>, LayerError> {
+  fn get_input_shape(&self) -> InputShape {
+    InputShape::Vector(self.biases.len())
+  }
+
+  fn get_output_shape(&self) -> OutputShape {
+    OutputShape::Vector(self.biases.len())
+  }
+
+  fn new(func: ActFunc) -> Self {
+    DenseLayer {
+      weights: Matrix::new(),
+      biases: Vec::new(),
+      func
+    }
+  }
+
+  fn init(
+    input_shape: InputShape, 
+    units: usize, 
+    func: ActFunc, 
+    method: InitMethod, 
+    seed: &mut u128
+  ) -> Result<Self, LayerInitError> {
+
+    match input_shape {
+      InputShape::Vector(inputs) => {
+        let mut body = Vec::with_capacity(units * inputs);
+        let mut biases = Vec::with_capacity(units);
+
+        match method {
+          InitMethod::Random => {
+            for _ in 0..units {
+              for _ in 0..inputs {
+                body.push(T::gen(seed));
+              }
+              biases.push(T::gen(seed));
+            }
+          },
+          _ => { return Err(LayerInitError::UnimplementedInitMethod) }
+        }
+
+        Ok(
+          DenseLayer {
+            weights: Matrix::from_body(body, [units, inputs]),
+            biases,
+            func
+          }
+        )
+      },
+      _ => { Err(LayerInitError::InvalidInputShape) }
+    }
+  }
+
+  /// Initializes a [`DenseLayer`] from an empty one.
+  fn init_mut(&mut self, 
+    input_shape: InputShape, 
+    units: usize, 
+    method: InitMethod, 
+    seed: &mut u128
+  ) -> Result<(), LayerInitError> {
+    
+    if self.is_empty() {
+      match input_shape {
+        InputShape::Vector(inputs) => {
+          let mut body = Vec::with_capacity(units * inputs);
+          let mut biases = Vec::with_capacity(units);
+    
+          match method {
+            InitMethod::Random => {
+              for _ in 0..units {
+                for _ in 0..inputs {
+                  body.push(T::gen(seed));
+                }
+                biases.push(T::gen(seed));
+              }
+    
+              self.weights = Matrix::from_body(body, [units, inputs]);
+              self.biases = biases;
+            },
+            _ => { return Err(LayerInitError::UnimplementedInitMethod); }
+          }
+        },
+        _ => { return Err(LayerInitError::InvalidInputShape); }
+      }
+    } else {
+      return Err(LayerInitError::AlreadyInitialized);
+    }
+
+    Ok(())
+  }
+
+  fn forward(&self, input_type: InputType<T>) -> Result<OutputType<T>, LayerForwardError> {
     match input_type {
       /* dense layer should receive a vector */
       InputType::Vector(input) => {
@@ -62,7 +136,7 @@ impl<T: Real + BasicOperations<T>> LayerLike<T> for DenseLayer<T> {
         /* layer returns a vector */
         Ok(OutputType::Vector(res))
       },
-      _ => { Err(LayerError::InvalidInput) }
+      _ => { Err(LayerForwardError::InvalidInput) }
     }
   }
 
--- src/rvnn/layer/mod.rs
+++ src/rvnn/layer/mod.rs
@@ -1,4 +1,4 @@
-use crate::input::{InputType, OutputType}; 
+use crate::input::{InputShape, InputType, OutputShape, OutputType}; 
 use crate::math::{BasicOperations, Real};
 use crate::act::ActFunc;
 
@@ -9,7 +9,7 @@ pub mod flatten;
 pub mod conv;
 
 #[derive(Debug)]
-pub enum LayerError {
+pub enum LayerForwardError {
   InvalidInput,
   UnimplementedLayer
 }
@@ -19,19 +19,33 @@ pub enum InitMethod {
   Distribution
 }
 
-pub enum InitError {
-  InvalidMethod
+#[derive(Debug)]
+pub enum LayerInitError {
+  InvalidInputShape,
+  UnimplementedInitMethod,
+  AlreadyInitialized
 }
 
 pub trait LayerLike<T> where Self: Sized {
+  fn is_empty(&self) -> bool;
 
-  fn init(inputs: usize, units: usize, func: ActFunc, method: InitMethod, seed: &mut u128) -> Result<Self, InitError>;
+  fn get_input_shape(&self) -> InputShape;
 
-  fn forward(&self, input: InputType<T>) -> Result<OutputType<T>, LayerError>;
+  fn get_output_shape(&self) -> OutputShape;
+
+  /// Initializes an empty layer
+  fn new(func: ActFunc) -> Self;
+
+  fn init(input_shape: InputShape, units: usize, func: ActFunc, method: InitMethod, seed: &mut u128) -> Result<Self, LayerInitError>;
+
+  fn init_mut(&mut self, input_shape: InputShape, units: usize, method: InitMethod, seed: &mut u128) -> Result<(), LayerInitError>;
+
+  fn forward(&self, input: InputType<T>) -> Result<OutputType<T>, LayerForwardError>;
 
   fn wrap(self) -> Layer<T>;
 }
 
+/// Just a general interface for a [`Network<T>`] that allows for a static personaliztion.
 pub enum Layer<T> {
   Dense(DenseLayer<T>),
   Flatten(Flatten<T>),
@@ -39,11 +53,35 @@ pub enum Layer<T> {
 }
 
 impl<T: Real + BasicOperations<T>> Layer<T> {
-  pub fn foward(&self, input_type: InputType<T>) -> Result<OutputType<T>, LayerError> {
+  pub fn is_empty(&self) -> bool {
+    match self {
+      Layer::Dense(l) => { l.is_empty() },
+      Layer::Flatten(l) => { l.is_empty() },
+      Layer::Convolutional(l) => { l.is_empty() }
+    }
+  }
+
+  pub fn get_input_shape(&self) -> InputShape {
+    match self {
+      Layer::Dense(l) => { l.get_input_shape() },
+      Layer::Flatten(l) => { l.get_input_shape() },
+      Layer::Convolutional(l) => { l.get_input_shape() }
+    }
+  }
+
+  pub fn get_output_shape(&self) -> OutputShape {
+    match self {
+      Layer::Dense(l) => { l.get_output_shape() },
+      Layer::Flatten(l) => { l.get_output_shape() },
+      Layer::Convolutional(l) => { l.get_output_shape() }
+    }
+  }
+
+  pub fn foward(&self, input_type: InputType<T>) -> Result<OutputType<T>, LayerForwardError> {
     /* deconstruct what type of layer it is */
     match self {
       Layer::Dense(l) => { l.forward(input_type) },
-      _ => { Err(LayerError::UnimplementedLayer) }
+      _ => { Err(LayerForwardError::UnimplementedLayer) }
     }
   }
 }
