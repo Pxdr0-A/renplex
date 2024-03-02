565285eba4bba6eddbd86e0dc94c45c526233c49
Pedro Alves <pedromfa98@gmail.com>
2024-03-02 00:25:47 +0000

    keeping track of unimplemented funcs

--- src/rvnn/layer/conv.rs
+++ src/rvnn/layer/conv.rs
@@ -1,7 +1,7 @@
 use crate::math::matrix::Matrix;
-use crate::input::{InputType, OutputType};
-
-use super::{InitError, LayerError, LayerLike};
+use crate::input::{InputShape, InputType, OutputType};
+use crate::act::ActFunc;
+use super::{LayerInitError, LayerForwardError, LayerLike};
 
 pub struct ConvLayer<T> {
   _weights: Matrix<T>,
@@ -9,11 +9,31 @@ pub struct ConvLayer<T> {
 }
 
 impl<T> LayerLike<T> for ConvLayer<T> {
-  fn init(inputs: usize, units: usize, func: crate::act::ActFunc, method: super::InitMethod, seed: &mut u128) -> Result<Self, InitError> {
+  fn is_empty(&self) -> bool {
+    unimplemented!()
+  }
+  
+  fn get_input_shape(&self) -> crate::input::InputShape {
     unimplemented!()
   }
 
-  fn forward(&self, _input_type: InputType<T>) -> Result<OutputType<T>, LayerError> {
+  fn get_output_shape(&self) -> crate::input::OutputShape {
+    unimplemented!()
+  }
+
+  fn new(func: ActFunc) -> Self {
+    unimplemented!()
+  }
+
+  fn init(input_shape: InputShape, units: usize, func: crate::act::ActFunc, method: super::InitMethod, seed: &mut u128) -> Result<Self, LayerInitError> {
+    unimplemented!()
+  }
+
+  fn init_mut(&mut self, input_shape: InputShape, units: usize, method: super::InitMethod, seed: &mut u128) -> Result<(), LayerInitError> {
+    unimplemented!()
+  }
+
+  fn forward(&self, _input_type: InputType<T>) -> Result<OutputType<T>, LayerForwardError> {
     unimplemented!()
   }
 
--- src/rvnn/layer/flatten.rs
+++ src/rvnn/layer/flatten.rs
@@ -1,19 +1,39 @@
 use std::marker::PhantomData;
 
-use crate::input::{InputType, OutputType};
-
-use super::{LayerError, LayerLike, InitError};
+use crate::input::{InputShape, InputType, OutputShape, OutputType};
+use crate::act::ActFunc;
+use super::{LayerForwardError, LayerLike, LayerInitError};
 
 pub struct Flatten<T> {
   _hidden: PhantomData<T> 
 }
 
 impl<T> LayerLike<T> for Flatten<T> {
-  fn init(inputs: usize, units: usize, func: crate::act::ActFunc, method: super::InitMethod, seed: &mut u128) -> Result<Self, InitError> {
+  fn is_empty(&self) -> bool {
     unimplemented!()
   }
 
-  fn forward(&self, _input_type: InputType<T>) -> Result<OutputType<T>, LayerError> {
+  fn get_input_shape(&self) -> InputShape {
+    unimplemented!()
+  }
+
+  fn get_output_shape(&self) -> OutputShape {
+    unimplemented!()
+  }
+
+  fn new(func: ActFunc) -> Self {
+    unimplemented!()
+  }
+
+  fn init(input_shape: InputShape, units: usize, func: crate::act::ActFunc, method: super::InitMethod, seed: &mut u128) -> Result<Self, LayerInitError> {
+    unimplemented!()
+  }
+
+  fn init_mut(&mut self, input_shape: InputShape, units: usize, method: super::InitMethod, seed: &mut u128) -> Result<(), LayerInitError> {
+    unimplemented!()
+  }
+
+  fn forward(&self, _input_type: InputType<T>) -> Result<OutputType<T>, LayerForwardError> {
     unimplemented!()
   }
 
