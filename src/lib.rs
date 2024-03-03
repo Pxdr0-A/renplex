3cbef38771c49dfeca29d7b075cd381ab3f711cc
Pedro Alves <pedromfa98@gmail.com>
2024-03-03 17:03:14 +0000

    tested complex networks

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod dataset;
 pub mod input;
 pub mod act;
 pub mod opt;
+pub mod init;
 pub mod rvnn;
 pub mod cvnn;
 
@@ -13,7 +14,8 @@ mod basic_tests {
 
   #[test]
   fn net_ops() {
-    use rvnn::layer::{InitMethod, LayerLike};
+    use init::InitMethod;
+    use rvnn::layer::LayerLike;
     use rvnn::layer::dense::DenseLayer;
     use act::ActFunc;
     use input::{IOShape, IOType};
@@ -48,6 +50,44 @@ mod basic_tests {
     println!("{:?}", net);
 
     let out = net.forward(IOType::Vector(vec![1.0; 2 * 4])).unwrap();
+
+    println!("{:?}", out);
+
+    use math::cfloat::Cf64;
+    use cvnn::layer::CLayerLike;
+    use cvnn::layer::dense::DenseCLayer;
+    use act::ComplexActFunc;
+    use cvnn::network::CNetwork;
+
+    let ref mut seed = 43827992_u128;
+    
+    let mut net = CNetwork::<Cf64>::new();
+
+    println!("{:?}", net);
+
+    net.add_input(
+      /* layer to be added (as input) */
+      DenseCLayer::new(ComplexActFunc::RITSigmoid).wrap(),
+      /* each neuron has 2 inputs */
+      IOShape::Vector(2),
+      /* 4 neurons in total */
+      4,
+      /* random initialization (with scale) */
+      InitMethod::Random(4), 
+      seed
+    ).unwrap();
+
+    net.add(
+      /* layer to be added (as hidden) */
+      DenseCLayer::new(ComplexActFunc::RITSigmoid).wrap(), 
+      2, 
+      InitMethod::Random(4), 
+      seed
+    ).unwrap();
+
+    println!("{:?}", net);
+
+    let out = net.forward(IOType::Vector(vec![Cf64 { x: 1.0, y: 1.0 }; 2 * 4])).unwrap();
 
     println!("{:?}", out);
   }
