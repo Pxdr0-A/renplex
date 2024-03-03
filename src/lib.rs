68c37d83622d53a633139bdb2c5ed45e05d1824a
Pedro Alves <pedromfa98@gmail.com>
2024-03-03 00:48:30 +0000

    tested basic network construction

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,48 @@ pub mod rvnn;
 pub mod cvnn;
 
 #[cfg(test)]
-mod matrix_tests {
+mod basic_tests {
 
-  #[test]
-  fn matrix_ops() {
-
-  }
+  use super::*;
 
   #[test]
   fn net_ops() {
-  
+    use rvnn::layer::{InitMethod, LayerLike};
+    use rvnn::layer::dense::DenseLayer;
+    use act::ActFunc;
+    use input::{IOShape, IOType};
+    use rvnn::network::Network;
+
+    let ref mut seed = 43827992_u128;
+    
+    let mut net = Network::<f32>::new();
+
+    println!("{:?}", net);
+
+    net.add_input(
+      /* layer to be added (as input) */
+      DenseLayer::new(ActFunc::Sigmoid).wrap(),
+      /* each neuron has 2 inputs */
+      IOShape::Vector(2),
+      /* 4 neurons in total */
+      4,
+      /* random initialization (with scale) */
+      InitMethod::Random(4), 
+      seed
+    ).unwrap();
+
+    net.add(
+      /* layer to be added (as hidden) */
+      DenseLayer::new(ActFunc::Sigmoid).wrap(), 
+      2, 
+      InitMethod::Random(4), 
+      seed
+    ).unwrap();
+
+    println!("{:?}", net);
+
+    let out = net.forward(IOType::Vector(vec![1.0; 2 * 4])).unwrap();
+
+    println!("{:?}", out);
   }
 }
