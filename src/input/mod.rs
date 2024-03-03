5426c08bf7eb8aeb00c4d2235c5bc9de4072f04b
Pedro Alves <pedromfa98@gmail.com>
2024-03-03 00:47:28 +0000

    removed boiler plate - no need for Input and Output

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,30 +1,12 @@
 use crate::math::matrix::Matrix;
 
-pub enum InputType<T> {
+#[derive(Debug)]
+pub enum IOType<T> {
   Vector(Vec<T>),
   Matrix(Matrix<T>)
 }
 
-pub enum OutputType<T> {
-  Vector(Vec<T>),
-  Matrix(Matrix<T>)
-}
-
-impl<T: Copy> OutputType<T> {
-  pub fn convert(&self) -> InputType<T> {
-    match self {
-      OutputType::Vector(v) => { InputType::Vector(v.clone()) },
-      OutputType::Matrix(m) => { InputType::Matrix(m.clone()) }
-    }
-  }
-}
-
-pub enum InputShape {
-  Vector(usize),
-  Matrix([usize; 2])
-}
-
-pub enum OutputShape {
+pub enum IOShape {
   Vector(usize),
   Matrix([usize; 2])
 }
