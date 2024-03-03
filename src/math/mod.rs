35f2aa897227d85bb4ec85c6e2928137a1da6a65
Pedro Alves <pedromfa98@gmail.com>
2024-03-03 00:48:10 +0000

    added a random gen method for both complex and real traits

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -14,14 +14,16 @@ impl<T, U> BasicOperations<T> for U where U: AddAssign + SubAssign + Add<Output=
 
 /// Trait containing utilities for RVNNs
 pub trait Real {
-  fn gen(seed: &mut u128) -> Self;
+  fn gen(seed: &mut u128, scale: usize) -> Self;
 
   fn sigmoid(self) -> Self;
 }
 
 impl Real for f32 {
-  fn gen(seed: &mut u128) -> Self {
-    lcgf32(seed)
+  fn gen(seed: &mut u128, scale: usize) -> Self {
+    let float_scale = scale as f32;
+
+    2.0 * float_scale * lcgf32(seed) - float_scale
   }
 
   fn sigmoid(self) -> Self {
@@ -29,8 +31,10 @@ impl Real for f32 {
   }
 }
 impl Real for f64 {
-  fn gen(seed: &mut u128) -> Self {
-    lcgf64(seed)
+  fn gen(seed: &mut u128, scale: usize) -> Self {
+    let float_scale = scale as f64;
+
+    2.0 * float_scale * lcgf64(seed) - float_scale
   }
 
   fn sigmoid(self) -> Self {
@@ -39,10 +43,21 @@ impl Real for f64 {
 }
 
 pub trait Complex {
+  fn gen(seed: &mut u128, scale: usize) -> Self;
+
   fn rit_sigmoid(self) -> Self;
 }
 
 impl Complex for Cf32 {
+  fn gen(seed: &mut u128, scale: usize) -> Self {
+    let float_scale = scale as f32;
+
+    Cf32 { 
+      x: 2.0 * float_scale * lcgf32(seed) - float_scale,
+      y: 2.0 * float_scale * lcgf32(seed) - float_scale
+    }
+  }
+
   fn rit_sigmoid(self) -> Self {
     Cf32 {
       x: self.x.exp() / (1.0 + self.y.exp()),
@@ -50,7 +65,17 @@ impl Complex for Cf32 {
     }
   }
 }
+
 impl Complex for Cf64 {
+  fn gen(seed: &mut u128, scale: usize) -> Self {
+    let float_scale = scale as f64;
+
+    Cf64 { 
+      x: 2.0 * float_scale * lcgf64(seed) - float_scale,
+      y: 2.0 * float_scale * lcgf64(seed) - float_scale
+    }
+  }
+
   fn rit_sigmoid(self) -> Self {
     Cf64 {
       x: self.x.exp() / (1.0 + self.y.exp()),
