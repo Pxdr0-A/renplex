f178d5307d266fee71617c3a78524b1e515fed72
Pedro Alves <pedromfa98@gmail.com>
2024-03-07 00:25:22 +0000

    added a small python env for data visualization

---
+++ cluster_viz.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+  df = pd.read_csv("data.csv")
+  
+  plt.figure()
+  for i in np.unique(df["class"]):
+    plt.plot(
+      df[df["class"] == i]["feature0"],
+      df[df["class"] == i]["feature1"],
+      "o"
+    )
+  
+  plt.show()
+
+if __name__ == "__main__":
+  main()
