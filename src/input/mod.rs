27bc895092b08a928533fc6e9db46658143502c0
Pedro Alves <pedromfa98@gmail.com>
2024-03-08 15:45:36 +0000

    added a release functionality

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,9 +1,30 @@
 use crate::math::matrix::Matrix;
 
 #[derive(Debug)]
+pub enum ReleaseError {
+  InvalidType
+}
+
+#[derive(Debug)]
 pub enum IOType<T> {
   Vector(Vec<T>),
   Matrix(Matrix<T>)
+}
+
+impl<T> IOType<T> {
+  pub fn release_vec(self) -> Result<Vec<T>, ReleaseError> {
+    match self {
+      IOType::Vector(vec) => { Ok(vec) },
+      _ => { Err(ReleaseError::InvalidType) }
+    }
+  }
+
+  pub fn release_mat(self) -> Result<Matrix<T>, ReleaseError> {
+    match self {
+      IOType::Matrix(mat) => { Ok(mat) },
+      _ => { Err(ReleaseError::InvalidType) }
+    }
+  }
 }
 
 #[derive(Debug)]
