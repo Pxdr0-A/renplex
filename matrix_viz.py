3919ef5d44790a443b10b370952c95eddb0e19ca
Pedro Alves <pedromfa98@gmail.com>
2024-04-03 23:37:43 +0100

    visualization of loss and accuracy curves

--- matrix_viz.py
+++ matrix_viz.py
@@ -3,10 +3,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-  df = pd.read_csv("matrix.csv", header=None, names=["vals"])
+  df_train_loss = pd.read_csv("out/lc_train_loss.csv", header=None, names=["vals"])
+  df_train_acc = pd.read_csv("out/lc_train_acc.csv", header=None, names=["vals"])
+  df_test_loss = pd.read_csv("out/lc_test_loss.csv", header=None, names=["vals"])
+  df_test_acc = pd.read_csv("out/lc_test_acc.csv", header=None, names=["vals"])
 
   plt.figure()
-  plt.plot(df["vals"], '.')
+  plt.plot(df_train_loss["vals"], '.')
+  plt.plot(df_test_loss["vals"], '.')
+
+  plt.figure()
+  plt.plot(df_train_acc["vals"], '.')
+  plt.plot(df_test_acc["vals"], '.')
+
   plt.show()
 
 if __name__ == "__main__":
