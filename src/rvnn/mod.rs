a5d53d2b68c184141175cf9c9eb63c3419fd9339
Pedro Alves <pedromfa98@gmail.com>
2024-04-13 16:34:48 +0100

    notes for what needs to be updated

--- src/rvnn/mod.rs
+++ src/rvnn/mod.rs
@@ -1,2 +1,9 @@
 pub mod layer;
 pub mod network;
+/* notes for conversion
+
+  remove the LayerLike trait
+  create conv layer
+  remove unecessary methods in the network module
+  add params_len method
+*/
