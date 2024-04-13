51e90fdb3b5bc77a6b98f7bf783e19afcd42828a
Pedro Alves <pedromfa98@gmail.com>
2024-04-13 16:35:26 +0100

    begining to define convolutional layer logic

--- src/cvnn/layer/conv.rs
+++ src/cvnn/layer/conv.rs
@@ -1,8 +1,14 @@
-use crate::{act::ComplexActFunc, math::matrix::Matrix};
+use crate::{act::ComplexActFunc, math::{matrix::Matrix, BasicOperations, Complex}};
 
 #[derive(Debug)]
 pub struct ConvCLayer<T> {
   weights: Vec<Matrix<T>>,
   bias: Matrix<T>,
   func: ComplexActFunc
+}
+
+impl<T: Complex + BasicOperations<T>> ConvCLayer<T> {
+  pub fn is_empty(&self) {
+    unimplemented!()
+  }
 }
