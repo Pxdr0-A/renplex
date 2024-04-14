79f3f858b7097f18eebbdbad7d478be289c6939b
Pedro Alves <pedromfa98@gmail.com>
2024-04-14 14:39:58 +0100

    added init method - not yet tested

--- src/cvnn/layer/conv.rs
+++ src/cvnn/layer/conv.rs
@@ -1,15 +1,15 @@
-use crate::{act::ComplexActFunc, err::LayerForwardError, input::{IOShape, IOType}, math::{matrix::Matrix, BasicOperations, Complex}};
+use crate::{act::ComplexActFunc, err::{LayerForwardError, LayerInitError}, init::InitMethod, input::{IOShape, IOType}, math::{matrix::Matrix, BasicOperations, Complex}};
 
 #[derive(Debug)]
 pub struct ConvCLayer<T> {
-  weights: Vec<Matrix<T>>,
+  kernels: Vec<Matrix<T>>,
   biases: Matrix<T>,
   func: ComplexActFunc
 }
 
 impl<T: Complex + BasicOperations<T>> ConvCLayer<T> {
   pub fn is_empty(&self) -> bool {
-    if self.weights.len() == 0 { true }
+    if self.kernels.len() == 0 { true }
     else { false }
   }
 
@@ -19,7 +19,7 @@ impl<T: Complex + BasicOperations<T>> ConvCLayer<T> {
 
   pub fn params_len(&self) -> (usize, usize) {
     let mut params: usize = 0;
-    for kernel in self.weights.iter() {
+    for kernel in self.kernels.iter() {
       let kernel_shape = kernel.get_shape();
       params += kernel_shape[0] * kernel_shape[1];
     }
@@ -39,12 +39,56 @@ impl<T: Complex + BasicOperations<T>> ConvCLayer<T> {
     IOShape::Matrix([bias_shape[0], bias_shape[1]])
   }
 
-  pub fn init() {
-    unimplemented!()
-  }
+  pub fn init(
+    input_shape: IOShape,
+    kernel_sizes: Vec<[usize; 2]>,
+    func: ComplexActFunc,
+    method: InitMethod,
+    seed: &mut u128
+  ) -> Result<Self, LayerInitError> {
 
-  pub fn init_mut() {
-    unimplemented!()
+    match input_shape {
+      IOShape::Matrix(dim) => {
+        let depth = kernel_sizes.len();
+        let mut kernels = Vec::with_capacity(depth);
+        let mut kernel = Vec::new();
+        let mut biases_body = Vec::with_capacity(dim[0] * dim[1]);
+
+        match method {
+          InitMethod::Random(scale) => {
+            for size in kernel_sizes.into_iter() {
+              if (size[0] - 1) / 2 >= dim[0] || (size[1] - 1) / 2 >= dim[1] {
+                return Err(LayerInitError::InvalidInputShape)
+              }
+    
+              for _ in 0..size[0] {
+                for _ in 0..size[1] {
+                  kernel.push(T::gen(seed, scale));
+                }
+              }
+
+              kernels.push(Matrix::from_body(kernel.clone(), size));
+              kernel.drain(..);
+            }
+
+            for _ in 0..dim[0] {
+              for _ in 0..dim[1] {
+                biases_body.push(T::gen(seed, scale));
+              }
+            }
+          }
+        }
+
+        Ok(
+          Self {
+            kernels,
+            biases: Matrix::from_body(biases_body, dim),
+            func
+          }
+        )
+      },
+      _ => { Err(LayerInitError::InvalidInputShape) }
+    }
   }
 
   pub fn trigger(&self, input_type: IOType<T>) -> Result<IOType<T>, LayerForwardError> {
