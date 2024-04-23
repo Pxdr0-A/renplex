9f9fa3014f2be40aa04f6f21e25ff3180c231852
Pedro Alves <pedromfa98@gmail.com>
2024-04-23 20:17:10 +0100

    tested rev_conv

--- image_viz.py
+++ image_viz.py
@@ -5,12 +5,15 @@ import numpy as np
 def main():
   df_image = pd.read_csv("./out/original.csv")
   df_image_conv = pd.read_csv("./out/conv_image.csv")
+  df_image_rev = pd.read_csv("./out/conv_image_rev.csv")
   df_image_conv1 = pd.read_csv("./out/conv_image1.csv")
 
   plt.figure()
   plt.imshow(df_image.values, cmap='hot', interpolation='nearest')
   plt.figure()
   plt.imshow(df_image_conv.values, cmap='hot', interpolation='nearest')
+  plt.figure()
+  plt.imshow(df_image_rev.values, cmap='hot', interpolation='nearest')
   plt.figure()
   plt.imshow(df_image_conv1.values, cmap='hot', interpolation='nearest')
   
