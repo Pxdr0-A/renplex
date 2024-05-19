60cec6003ad3458f342d12fab770027c9f003899
Pedro Alves <pedromfa98@gmail.com>
2024-05-19 19:00:43 +0100

    further sloopy visualization

--- image_viz.py
+++ image_viz.py
@@ -1,24 +1,59 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import ast
+
+def filter_complex_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+  cols = df.columns
+  df0 = pd.DataFrame()
+  for index in range(0, len(cols)-1, 2):
+    df0[str(index)] = [ast.literal_eval(s) for s in df[cols[index]] + "," + df[cols[index+1]]]
+
+  cols0 = df0.columns
+  for col0 in cols0:
+    df0[col0] = df0[col0].apply(lambda x: np.linalg.norm(x))
+
+  return df0
 
 def main():
-  df_image = pd.read_csv("./out/original.csv")
-  df_image_conv = pd.read_csv("./out/conv_image.csv")
-  df_image_rev = pd.read_csv("./out/conv_image_rev.csv")
-  df_image_rev1 = pd.read_csv("./out/conv_image_rev1.csv")
-  df_image_conv1 = pd.read_csv("./out/conv_image1.csv")
-
+  df_image = pd.read_csv("./out/complex_features/lr_conv1_1_0_20e_original.csv")
+  df_feature0 = pd.read_csv("./out/complex_features/lr_conv1_1_0_20e_feature_0.csv")
+  df_feature1 = pd.read_csv("./out/complex_features/lr_conv1_1_0_20e_feature_1.csv")
+  df_feature2 = pd.read_csv("./out/complex_features/lr_conv1_1_0_20e_feature_2.csv")  
+  df_feature3 = pd.read_csv("./out/complex_features/lr_conv1_1_0_20e_feature_3.csv")  
+  df_feature4 = pd.read_csv("./out/complex_features/lr_conv1_1_0_20e_feature_4.csv")
+  df_feature5 = pd.read_csv("./out/complex_features/lr_conv1_1_0_20e_feature_5.csv")  
+  df_feature6 = pd.read_csv("./out/complex_features/lr_conv1_1_0_20e_feature_6.csv")
+  df_feature7 = pd.read_csv("./out/complex_features/lr_conv1_1_0_20e_feature_7.csv") 
+  
+  df0 = filter_complex_dataframe(df_image)
+  df1 = filter_complex_dataframe(df_feature0)
+  df2 = filter_complex_dataframe(df_feature1)
+  df3 = filter_complex_dataframe(df_feature2)
+  df4 = filter_complex_dataframe(df_feature3)
+  df5 = filter_complex_dataframe(df_feature4)
+  df6 = filter_complex_dataframe(df_feature5)
+  df7 = filter_complex_dataframe(df_feature6)
+  df8 = filter_complex_dataframe(df_feature7)
+  
   plt.figure()
-  plt.imshow(df_image.values, cmap='hot', interpolation='nearest')
+  plt.imshow(df0.values, cmap='hot', interpolation='nearest')
   plt.figure()
-  plt.imshow(df_image_conv.values, cmap='hot', interpolation='nearest')
+  plt.imshow(df1.values, cmap='hot', interpolation='nearest')
   plt.figure()
-  plt.imshow(df_image_rev.values, cmap='hot', interpolation='nearest')
+  plt.imshow(df2.values, cmap='hot', interpolation='nearest')
   plt.figure()
-  plt.imshow(df_image_rev1.values, cmap='hot', interpolation='nearest')
+  plt.imshow(df3.values, cmap='hot', interpolation='nearest')
   plt.figure()
-  plt.imshow(df_image_conv1.values, cmap='hot', interpolation='nearest')
+  plt.imshow(df4.values, cmap='hot', interpolation='nearest')
+  plt.figure()
+  plt.imshow(df5.values, cmap='hot', interpolation='nearest')
+  plt.figure()
+  plt.imshow(df6.values, cmap='hot', interpolation='nearest')
+  plt.figure()
+  plt.imshow(df7.values, cmap='hot', interpolation='nearest')
+  plt.figure()
+  plt.imshow(df8.values, cmap='hot', interpolation='nearest')
   
   plt.show()
 
--- loss_acc_curves.py
+++ loss_acc_curves.py
@@ -2,21 +2,43 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-def main():
-  df_train_loss = pd.read_csv("out/conv_network_loss.csv", header=None, names=["vals"])
-  df_train_acc = pd.read_csv("out/conv_network_acc.csv", header=None, names=["vals"])
-  df_test_loss = pd.read_csv("out/conv_network_test_loss.csv", header=None, names=["vals"])
-  df_test_acc = pd.read_csv("out/conv_network_test_acc.csv", header=None, names=["vals"])
-
+def lr_studies_dense():
+  lr_list = [
+    #"0.1", "0.5",
+    #"1", "1.4", "1.7", 
+    #"2", "2.5", "2.7",
+    #"3", "3.3", "3.7",
+    # Start of Optimal Regime
+    "4", "4.3", "4.7",
+    "5", "5.3", "5.7",
+    "6", "6.3", "6.7",
+    "7", "7.3",
+    # A bit caotic but still good results 
+    "7.7", "8", "8.5",
+    # End of Optimal Regime
+    #"9", "9.5", "10"
+  ]
+  
   plt.figure()
-  plt.plot(df_train_loss["vals"], '.')
-  plt.plot(df_test_loss["vals"], '.')
+  for lr in lr_list:
+    #df_train_loss = pd.read_csv(f"out/loss_{lr}_0.csv", header=None, names=["vals"])
+    #df_train_acc = pd.read_csv(f"out/acc_{lr}_0.csv", header=None, names=["vals"])
+    #df_test_loss = pd.read_csv(f"out/test_loss_{lr}_0.csv", header=None, names=["vals"])
+    df_test_acc = pd.read_csv(f"out/lr_studies_/test_acc_{lr}_0.csv", header=None, names=["vals"])
 
-  plt.figure()
-  plt.plot(df_train_acc["vals"], '.')
-  plt.plot(df_test_acc["vals"], '.')
+    #plt.figure()
+    #plt.plot(df_train_loss["vals"], '.')
+    #plt.plot(df_test_loss["vals"], '.')
 
+    #plt.figure()
+    #plt.plot(df_train_acc["vals"], '.')
+    plt.plot(df_test_acc["vals"], '-o', label=lr)
+
+  plt.legend()
   plt.show()
+
+def main():
+  pass
 
 if __name__ == "__main__":
   main()
