4a5af96a2f170a64c9ef661d5b3212b3906f9cd9
Pedro Alves <pedromfa98@gmail.com>
2024-06-10 19:50:36 +0100

    refined data viz

--- loss_acc_curves.py
+++ loss_acc_curves.py
@@ -4,41 +4,34 @@ import numpy as np
 
 def lr_studies_dense():
   lr_list = [
-    #"0.1", "0.5",
-    #"1", "1.4", "1.7", 
-    #"2", "2.5", "2.7",
-    #"3", "3.3", "3.7",
-    # Start of Optimal Regime
-    "4", "4.3", "4.7",
-    "5", "5.3", "5.7",
-    "6", "6.3", "6.7",
-    "7", "7.3",
-    # A bit caotic but still good results 
-    "7.7", "8", "8.5",
-    # End of Optimal Regime
-    #"9", "9.5", "10"
+    "1.00_0.00",
+    "2.00_0.00",
+    "3.00_0.00",
+    "4.00_0.00",
   ]
   
-  plt.figure()
+  f1 = plt.figure()
+  f2 = plt.figure()
+  ax1 = f1.add_subplot(111)
+  ax2 = f2.add_subplot(111)
   for lr in lr_list:
     #df_train_loss = pd.read_csv(f"out/loss_{lr}_0.csv", header=None, names=["vals"])
     #df_train_acc = pd.read_csv(f"out/acc_{lr}_0.csv", header=None, names=["vals"])
-    #df_test_loss = pd.read_csv(f"out/test_loss_{lr}_0.csv", header=None, names=["vals"])
-    df_test_acc = pd.read_csv(f"out/lr_studies_/test_acc_{lr}_0.csv", header=None, names=["vals"])
-
-    #plt.figure()
+    df_test_loss = pd.read_csv(f"out/0/test_loss_{lr}_20e.csv", header=None, names=["vals", "end"])
+    df_test_accu = pd.read_csv(f"out/0/test_acc_{lr}_20e.csv", header=None, names=["vals", "end"])
+    
     #plt.plot(df_train_loss["vals"], '.')
-    #plt.plot(df_test_loss["vals"], '.')
+    ax1.plot(df_test_loss["vals"], '-o', label=lr)
 
-    #plt.figure()
     #plt.plot(df_train_acc["vals"], '.')
-    plt.plot(df_test_acc["vals"], '-o', label=lr)
+    ax2.plot(df_test_accu["vals"], '-o', label=lr)
 
-  plt.legend()
+  ax1.legend()
+  ax2.legend()
   plt.show()
 
 def main():
-  pass
+  lr_studies_dense()
 
 if __name__ == "__main__":
   main()
