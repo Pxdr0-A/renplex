4704f8431c7997f1fbbbd424cc44e1619d14dad6
Pedro Alves <pedromfa98@gmail.com>
2024-11-28 12:18:16 +0100

    added a build file to point to lib directory of amd optimized math subprograms

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-  // attempting to connect fortran statically
-  // for now it is not working
-  println!("cargo:rustc-link-search=native=/usr/lib/gcc/x86_64-redhat-linux/14");
+    // attempting to connect fortran statically
+    // for now it is not working
+    println!("cargo:rustc-link-search=native=/opt/AMD/aocl/aocl-linux-gcc-5.0.0/gcc/lib");
 }
