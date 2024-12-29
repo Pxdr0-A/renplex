2a6e7a3ecf83ca10a291b5c0e4fd22eacce68a4c
Pedro Alves <pedromfa98@gmail.com>
2024-12-29 16:52:05 +0100

    advanced with proc macro for impl supermodule - dynamic args in method implementation

---
+++ renplex_derive/src/lib.rs
@@ -0,0 +1,60 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{Data, Fields, Ident, Type};
+
+#[proc_macro_derive(SuperModuleMacro)]
+pub fn super_module_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+
+    impl_supermodule_macro(&ast)
+}
+
+fn impl_supermodule_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let field_idents = if let Data::Struct(data_struct) = &ast.data {
+        match &data_struct.fields {
+            Fields::Named(fields_named) => fields_named
+                .named
+                .iter()
+                .filter_map(|field| field.ident.clone())
+                .collect::<Vec<Ident>>(),
+            _ => Vec::new(),
+        }
+    } else {
+        Vec::new()
+    };
+
+    let _field_tys = if let Data::Struct(data_struct) = &ast.data {
+        match &data_struct.fields {
+            Fields::Named(fields_named) => fields_named
+                .named
+                .iter()
+                .map(|field| field.ty.clone())
+                .collect::<Vec<Type>>(),
+            _ => Vec::new(),
+        }
+    } else {
+        Vec::new()
+    };
+
+    // pre-processing token streams for the implementation
+
+    // init method arguments
+    let init_args = field_idents.iter().map(|field| {
+        let arg_name = field;
+        let arg_type = quote! { HashMap<String, String> };
+
+        quote! { #arg_name: #arg_type }
+    });
+
+    // implement functionalities
+    let gen = quote! {
+        impl #name {
+            pub fn init(#(#init_args),*) {
+                println!("Initialized supermodule.")
+            }
+        }
+    };
+
+    gen.into()
+}
