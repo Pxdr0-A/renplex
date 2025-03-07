ba9fda64770b4f9c34d028a06b5328144f50d02d
Pedro Alves <pedromfa98@gmail.com>
2025-03-07 09:37:45 +0100

    further steps with initialization - need to add it to ModuleCore

--- renplex_core/src/lib.rs
+++ renplex_core/src/lib.rs
@@ -1,5 +1,6 @@
 use std::{collections::HashMap, marker::PhantomData};
 
+use activations::Activation;
 use num_complex::Complex32;
 use tensor::{ComplexRoutines, StaticTensor, Tensor};
 // std and dependencies imports
@@ -66,16 +67,23 @@ pub mod tensor {
             unimplemented!()
         }
 
-        fn core_ref(&self) -> (&[C], &[usize]) {
+        fn core_ref(&self) -> (&[Self::C], &[usize]) {
             (self._array.as_slice(), self._shape.as_slice())
         }
     }
 }
 
+type InitArgs = HashMap<&'static str, &'static str>;
 // generic functions for defining forward passes like Linear or Convolutional
 // might not be a bad idea to have another generic type for the bias
 pub trait ModuleCore {
-    fn compute<TW: Tensor, TO: Tensor, TI: Tensor>(weights: &TW, bias: &TO, input: &TI) -> TO;
+    fn init<TW: Tensor, TB: Tensor>(args: &InitArgs) -> (TW, TB);
+
+    fn compute<TW: Tensor, TB: Tensor, TI: Tensor, TO: Tensor>(
+        weights: &TW,
+        bias: &TB,
+        input: &TI,
+    ) -> TO;
 
     // for the derivatives and so on
 }
@@ -83,56 +91,71 @@ pub trait ModuleCore {
 pub struct LinearCore;
 
 impl ModuleCore for LinearCore {
-    fn compute<TW: Tensor, TO: Tensor, TI: Tensor>(weights: &TW, bias: &TO, input: &TI) -> TO {
+    fn init<TW: Tensor, TB: Tensor>(args: &InitArgs) -> (TW, TB) {
+        unimplemented!()
+    }
+
+    fn compute<TW: Tensor, TB: Tensor, TI: Tensor, TO: Tensor>(
+        weights: &TW,
+        bias: &TB,
+        input: &TI,
+    ) -> TO {
         unimplemented!()
     }
 }
 
 pub trait Module {
     type Weight: Tensor;
+    type Bias: Tensor;
     type Output: Tensor;
+
+    // not right?! MAYBE EQUIVALENT BUT INSTED OF MODULECORE, INITILIZATION TRAIT WITH GENERIC
+    // THE INITIALIZATION CANNOT RECEIVE THE ARGS BECAUSE IT DOES NOT DISTINGUISH LAYERS
+    // THEN, INITIALIZATION TYPE NEEDS TO BE ACCESSED BY THE MODULE CORE
+    fn init(args: &InitArgs) -> Self;
 
     fn forward<T: Tensor>(&self, input: &T) -> Self::Output;
 
     fn backward<T: Tensor>(&self, grad: &Self::Output, input: &T) -> T;
 
-    fn gradient<T: Tensor>(&self, grad: &Self::Output, input: &T) -> (Self::Weight, Self::Output);
+    fn gradient<T: Tensor>(&self, grad: &Self::Output, input: &T) -> (Self::Weight, Self::Bias);
 }
 
-pub struct GeneralModule<C, TW, TO>
+pub struct GeneralModule<C, TW, TB, TO>
 where
     C: ModuleCore,
     TW: Tensor,
+    TB: Tensor,
     TO: Tensor,
 {
-    core: C,
+    core: PhantomData<C>,
     weights: TW,
-    bias: TO,
+    bias: TB,
+    output: PhantomData<TO>,
 }
 
-pub type StaticLinear<C, const LENW: usize, const LENO: usize> =
-    GeneralModule<LinearCore, StaticTensor<C, LENW>, StaticTensor<C, LENO>>;
-
-// define here more types
-pub struct GeneralLayer<C, TW, TO>
+impl<C, TW, TB, TO> Module for GeneralModule<C, TW, TB, TO>
 where
     C: ModuleCore,
     TW: Tensor,
-    TO: Tensor,
-{
-    plain_layer: GeneralModule<C, TW, TO>,
-    activation: PhantomData<TW>,
-}
-
-impl<C, TW, TO> Module for GeneralModule<C, TW, TO>
-where
-    C: ModuleCore,
-    TW: Tensor,
+    TB: Tensor,
     TO: Tensor,
 {
     // This one seems to be needed but I do not know about the others?
     type Weight = TW;
+    type Bias = TB;
     type Output = TO;
+
+    fn init(args: &InitArgs) -> Self {
+        let (weights, bias) = C::init(args);
+
+        Self {
+            core: PhantomData,
+            weights,
+            bias,
+            output: PhantomData,
+        }
+    }
 
     fn forward<T: Tensor>(&self, input: &T) -> Self::Output {
         let (weights, bias) = (&self.weights, &self.bias);
@@ -143,9 +166,30 @@ where
         unimplemented!()
     }
 
-    fn gradient<T: Tensor>(&self, grad: &Self::Output, input: &T) -> (Self::Weight, Self::Output) {
+    fn gradient<T: Tensor>(&self, grad: &Self::Output, input: &T) -> (Self::Weight, Self::Bias) {
         unimplemented!()
     }
+}
+
+// THE WAY LAYERS ARE INITIALIZED DEPENDS ON THE MODULECORE (or maybe not)! MAYBE IT WILL TAKE CARE OF THAT.
+// HOWEVER, Module can still have the init!
+// initialization will receive some args and return the tensors.
+// BIAS MIGHT NEED ITS OWN TYPE!!! TRY THAT!!!
+
+pub type StaticLinear<C, const LENW: usize, const LENB: usize, const LENO: usize> =
+    GeneralModule<LinearCore, StaticTensor<C, LENW>, StaticTensor<C, LENB>, StaticTensor<C, LENO>>;
+
+// define here more types
+pub struct GeneralLayer<C, A, TW, TB, TO>
+where
+    C: ModuleCore,
+    A: Activation,
+    TW: Tensor,
+    TB: Tensor,
+    TO: Tensor,
+{
+    plain_layer: GeneralModule<C, TW, TB, TO>,
+    activation: A,
 }
 
 // TACKLE THIS NOW!!
@@ -157,6 +201,7 @@ pub mod init {
 pub mod optimization {
     use crate::{tensor::Tensor, Module};
 
+    // I think this is alright
     pub trait Optimizer {
         fn step<M: Module, W: Tensor, B: Tensor>(&mut self, module: &mut M, gradw: &W, gradb: &B);
     }
@@ -165,9 +210,11 @@ pub mod optimization {
 pub mod modules {}
 
 pub mod activations {
-    use crate::tensor::Tensor;
+    use crate::{tensor::Tensor, InitArgs};
 
     pub trait Activation {
+        fn init(args: &InitArgs) -> Self;
+
         fn update_params(&mut self);
 
         fn compute<T: Tensor>(&self, input: &mut T);
@@ -178,6 +225,31 @@ pub mod activations {
     pub struct Tanh;
 
     impl Activation for Tanh {
+        fn init(_args: &InitArgs) -> Self {
+            Tanh
+        }
+
+        fn update_params(&mut self) {}
+
+        fn compute<T: Tensor>(&self, input: &mut T) {
+            unimplemented!()
+        }
+
+        fn derivative<T: Tensor>(&self, input: &mut T) {
+            unimplemented!()
+        }
+    }
+
+    pub struct RawDropout {
+        config: Vec<usize>,
+        prob: f32,
+    }
+
+    impl Activation for RawDropout {
+        fn init(args: &InitArgs) -> Self {
+            unimplemented!()
+        }
+
         fn update_params(&mut self) {
             unimplemented!()
         }
@@ -191,11 +263,17 @@ pub mod activations {
         }
     }
 
-    pub struct Dropout<P> {
-        parameters: P,
+    pub struct Dropout<A: Activation> {
+        pre_act: A,
+        config: Vec<usize>,
+        prob: f32,
     }
 
-    impl<P> Activation for Dropout<P> {
+    impl<A: Activation> Activation for Dropout<A> {
+        fn init(args: &InitArgs) -> Self {
+            unimplemented!()
+        }
+
         fn update_params(&mut self) {
             unimplemented!()
         }
